74f0ec9d3c77e21d96aadc51c6700cab65f6d76e
MELANIE Griffiths <mg245439@falmouth.ac.uk>
2021-10-12 18:14:14 +0100

    Added a simple wandering AI code

---
+++ Assets/Scripts/AI Scripts/WanderScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderScript : MonoBehaviour
+{
+    [SerializeField]
+    private float wanderRadius;
+    [SerializeField]
+    private float wanderTimer;
+
+    private Transform targetLoc;
+    private NavMeshAgent agent;
+    private float timer;
+
+    // this function is called when the object is enabled
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; /// increases timer every second
+
+        if(timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layerMask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layerMask);
+
+        return navHit.position;
+    }
+}
