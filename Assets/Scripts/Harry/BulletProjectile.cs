70d91a119beb882bef26607cae45e5ba42697407
GEORGI ALEKSANDROV <ga246525@falmouth.ac.uk>
2021-11-23 17:51:02 +0000

    Creating new bullet Prefabs Re done the wait before self destruct Bullets now stick to non enemies

--- Assets/Scripts/Harry/BulletProjectile.cs
+++ Assets/Scripts/Harry/BulletProjectile.cs
@@ -9,18 +9,39 @@ public class BulletProjectile : MonoBehaviour
     [HideInInspector]
     public float damage;
 
+    public float thisLifespan = 5f;
+
+    private float currentAge;
+
     private void Start()
     {
         float speed = 20f;
         bulletRigidbody.velocity = transform.forward * speed;
     }
+
+    private void Update()
+    {
+        currentAge += Time.deltaTime;
+
+        if (currentAge >= thisLifespan)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-
+        
         if (other.tag == "Enemy") {
+            Destroy(gameObject);
             EnemyStatisticsManager enemyStatisticsManager = other.gameObject.GetComponent<EnemyStatisticsManager>();
             enemyStatisticsManager.TakeDamage(damage);
+        }
+        else
+        {
+            bulletRigidbody.velocity = Vector3.zero;
+            currentAge = 0;
+            bulletRigidbody.isKinematic = true;
         }
     }
 }
