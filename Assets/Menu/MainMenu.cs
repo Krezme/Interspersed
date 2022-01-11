21061d11f0394b06f502c9280bec129145c82aad
GA246525 <ga246525@falmouth.ac.uk>
2022-01-11 22:49:33 +0000

    CutScenes

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -5,10 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("ImprovedLMALevel");
     }
+
+    public void LoadCutScene1() {
+        SceneManager.LoadScene("CutScene1");
+    }
+    public void LoadCutScene2() {
+        SceneManager.LoadScene("CutScene2");
+    }
+    public void LoadCutScene3() {
+        SceneManager.LoadScene("CutScene3");
+    }
+    public void LoadCutScene4() {
+        SceneManager.LoadScene("CutScene4");
+    }
+    public void LoadCutScene5() {
+        SceneManager.LoadScene("CutScene5");
+    }
+    public void LoadCutScene6() {
+        SceneManager.LoadScene("CutScene6");
+    }
+
     public void ExitGame()
     {
         Application.Quit();
