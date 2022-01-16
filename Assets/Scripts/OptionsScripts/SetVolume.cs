1d2e8a3b492badc85e20d96b17219e3a589412d6
MELANIE Griffiths <mg245439@falmouth.ac.uk>
2022-01-16 20:13:21 +0000

    Added Music and Sound Effects volume bars
    - Programming has been set up for both but as the SFX group has not been added yet, the sound effects bar does nothing

--- Assets/Scripts/OptionsScripts/SetVolume.cs
+++ Assets/Scripts/OptionsScripts/SetVolume.cs
@@ -7,8 +7,18 @@ public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
 
-    public void SetLevel(float sliderValue)
+    public void SetMasterVolLevel(float sliderValue)
     {
         mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSFXLevel(float sliderValue)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
     }
 }
