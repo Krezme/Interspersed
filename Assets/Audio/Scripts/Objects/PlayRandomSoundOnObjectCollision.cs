2a024f3b4033a31befa9459f4c47cf6313f9ab4d
Rhys Lampard <rhyslampard@gmail.com>
2022-01-27 15:34:32 +0000

    Object Impact sounds
    Added 'PlayRandomSoundOnObjectCollision' script
                -Implemented RandomAudioPlayer inspector window slot
                -Implemented 'RequiredVelocity' variable to alter what velocity is required for
                 a sound to play
    Added ObjectCollisionTestObject

---
+++ Assets/Audio/Scripts/Objects/PlayRandomSoundOnObjectCollision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamekit3D //The RandomAudioPlayer scipt was originally copied from Unity's 3DGameKit 'The Explorer' and to my knowledge, requires this namespace to function
+{
+    public class PlayRandomSoundOnObjectCollision : MonoBehaviour
+    {
+        public RandomAudioPlayer player;
+        public float RequiredVelocity;
+        //Creates inspector window slot in which the GameObject that contains the desired to be played RandomAudioPlayer Sctipt must be placed (In this case it should be tbe object that this script is also placed on)
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.relativeVelocity.magnitude > RequiredVelocity)
+            {
+                player.PlayRandomClip();
+                //Initiates the RandomAudioPlayer script within the assigned GameObject via the inspector window slot to play a random sound from its default bank
+            }
+        }
+    }
+}
