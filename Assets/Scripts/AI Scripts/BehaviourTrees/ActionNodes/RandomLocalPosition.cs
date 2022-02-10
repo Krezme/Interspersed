08681da1c618bd138b38c85d254d0ab5c1cd8076
GA246525 <ga246525@falmouth.ac.uk>
2022-02-10 01:31:06 +0000

    Baked OcclusionCulling and Navmesh and testing for bugs

--- Assets/Scripts/AI Scripts/BehaviourTrees/ActionNodes/RandomLocalPosition.cs
+++ Assets/Scripts/AI Scripts/BehaviourTrees/ActionNodes/RandomLocalPosition.cs
@@ -18,8 +18,15 @@ public class RandomLocalPosition : ActionNode
     }
 
     protected override State OnUpdate() {
-        Vector3 newDest = new Vector3(Random.Range(-wanderRange, wanderRange) + context.transform.position.x, 0f, Random.Range(-wanderRange, wanderRange) + context.transform.position.z);
-        blackboard.moveToPosition = newDest;
-        return State.Success;
+        //Vector3 newDest = new Vector3(Random.Range(-wanderRange, wanderRange) + context.transform.position.x, 0f, Random.Range(-wanderRange, wanderRange) + context.transform.position.z);
+        Vector3 newDest;
+        Vector3 randomPoint = context.transform.position + Random.insideUnitSphere * wanderRange; /// picks a random location within the sphere
+        if(NavMesh.SamplePosition(randomPoint, out navHit, 1.0f, NavMesh.AllAreas)) /// Finds a suitable place to walk to
+        {
+            newDest = navHit.position;
+            blackboard.moveToPosition = newDest;
+            return State.Success;
+        }
+        return State.Running;
     }
 }
