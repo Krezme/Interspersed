fd049e79f39aea8b7c19a09551b5a059aa950bbb
Rhys Lampard <rhyslampard@gmail.com>
2022-03-03 13:10:57 +0000

    Checkpoint Sounds!
    -Added Bell Ring sound to play when checkpoint is activated
    -Added Tune to play when checkpoint is activated
    -Modified 'Checkpoint' Script to add logic to play sounds on checkpoint activation

--- Assets/Scripts/Enviroment/Checkpoint.cs
+++ Assets/Scripts/Enviroment/Checkpoint.cs
@@ -12,6 +12,8 @@ public class Checkpoint : MonoBehaviour
     public Light[] lights;
     public Transform playerSpawnPos;
     public Vector3 offset = new Vector3 (0,1,0);
+    public RandomAudioPlayer Bell;
+    public RandomAudioPlayer Tune;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +40,9 @@ public class Checkpoint : MonoBehaviour
             light.enabled = true;
         }
         this.gameObject.GetComponent<Collider>().enabled = false;
+
+        Bell.PlayRandomClip(); //Rhys - Plays Telephone ring sound within the selected Phone Box on activation
+        Tune.PlayRandomClip(); //Rhys - Plays checkpoint activation melody
     }
 
     public void DeselectCheckpoint () {
