6dc1769a619f7a47f92b2097de97e5d19a93ce6c
GEORGI ALEKSANDROV <Ga246525@falmouth.ac.uk>
2022-03-03 16:36:36 +0000

    When the player falls from the world they get teleported back to a checkpoint

---
+++ Assets/PlayerFallenInVoid.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFallenInVoid : MonoBehaviour
+{
+    public float verticalFallingDistance;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (ThirdPersonPlayerController.instance.gameObject.transform.position.y <= verticalFallingDistance)
+        {
+            ThirdPersonPlayerController.instance.gameObject.transform.position = CheckpointManager.instance.checkpoints[CheckpointManager.instance.currentCheckpointIndex].playerSpawnPos.position + CheckpointManager.instance.checkpoints[CheckpointManager.instance.currentCheckpointIndex].offset;
+        }
+    }
+}
--- Assets/Scripts/Enviroment/MovePlayerToCheckpoint.cs
+++ Assets/Scripts/Enviroment/MovePlayerToCheckpoint.cs
@@ -6,8 +6,18 @@ public class MovePlayerToCheckpoint : MonoBehaviour
 {
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
-            Debug.Log("APIFJIAEJFIPJAEPFJAOPEJFOPAJE");
+            Debug.Log(ThirdPersonPlayerController.instance.gameObject.transform.position);
             ThirdPersonPlayerController.instance.gameObject.transform.position = CheckpointManager.instance.checkpoints[CheckpointManager.instance.currentCheckpointIndex].playerSpawnPos.position + CheckpointManager.instance.checkpoints[CheckpointManager.instance.currentCheckpointIndex].offset;
+            ThirdPersonPlayerController.instance.verticalVelocity = 0;
+            Debug.Log(ThirdPersonPlayerController.instance.gameObject.transform.position);
         }
     }
+
+    /*void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ThirdPersonPlayerController.instance.gameObject.transform.position = CheckpointManager.instance.checkpoints[CheckpointManager.instance.currentCheckpointIndex].playerSpawnPos.position + CheckpointManager.instance.checkpoints[CheckpointManager.instance.currentCheckpointIndex].offset;
+        }
+    }*/
 }
