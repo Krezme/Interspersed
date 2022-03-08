9b3e63380c5da7404a6ae1c8220af7e5016cbc02
OTTO JACOBI <OJ247434@falmouth.ac.uk>
2022-03-08 14:40:56 +0000

    added the kinetic enemy's functionality in KineticEnemyScript2, it now throws physics objects at the player.
    specifically the furthest object it can throw.

--- Assets/Scripts/AI Scripts/KineticEnemyScript2.cs
+++ Assets/Scripts/AI Scripts/KineticEnemyScript2.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class KineticEnemyScript2 : MonoBehaviour
 {
@@ -9,45 +10,130 @@ public class KineticEnemyScript2 : MonoBehaviour
     private List<float> objectDistances = new List<float>();
 
     public GameObject playerVar;
-   
+    private bool playerInRange = false;
+    private bool canThrow = false;
+
+    public GameObject headObject;
+
+    public GameObject object2Throw;
+
+    public float throwCooldownTime = 2f;
+    public float throwForce = 1f;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        findDistance();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void findDistance()
     {
-        foreach(GameObject physicsObject in physicsObjects)
+ 
+        //sort object list
+
+        var PObjects = physicsObjects;
+        var sortedPObjects = PObjects.OrderBy(obj => Vector3.Distance(playerVar.transform.position, obj.transform.position));
+
+        GameObject[] sortedPObjectsArray = sortedPObjects.ToArray();
+
+        object2Throw = sortedPObjectsArray[sortedPObjectsArray.Length - 1];
+
+
+    }
+
+    private void OnTriggerEnter(Collider other) //enter
+    {
+        if (other.gameObject.tag == "Player")
         {
-            float dist = Vector3.Distance(gameObject.transform.position, playerVar.transform.position);
+            playerInRange = true;
 
-            objectDistances.Add(dist);
+            Debug.Log("Player Entered");
+
+            playerVar = other.gameObject;
+
+            StartCoroutine(throwCooldownFunc());
+
         }
-
-
-        objectDistances.Sort();
-
-        foreach(float f in objectDistances)
+        else
         {
-            Debug.Log(f);
+            if(other.attachedRigidbody.isKinematic == false)
+            {
+                physicsObjects.Add(other.gameObject);
+            }
         }
-
-        Debug.Log("greatestDistance: " + objectDistances[physicsObjects.Count - 1]);
 
 
 
     }
 
+    private void OnTriggerExit(Collider other) //exit
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+
+            Debug.Log("Player Left");
+
+            playerVar = null;
+
+
+        }
+        else
+        {
+            if (other.attachedRigidbody.isKinematic == false)
+            {
+                physicsObjects.Remove(other.gameObject);
+            }
+        }
+    }
+
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            headObject.transform.LookAt(other.gameObject.transform);
+
+            if (canThrow)
+            {
+                throwObjectFunc();
+            }
+        }
+    }
+
+
+    private void throwObjectFunc()
+    {
+        StartCoroutine(throwCooldownFunc());
+        findDistance();
+
+        object2Throw.GetComponent<Rigidbody>().AddForce((playerVar.transform.position - object2Throw.transform.position) * throwForce);
+
+
+
+    }
+
+    IEnumerator throwCooldownFunc()
+    {
+        Debug.Log("Throw Charging");
+
+
+        canThrow = false;
+        yield return new WaitForSeconds(throwCooldownTime);
+        canThrow = true;
+
+        Debug.Log("Can Throw");
+
+
+    }
 
 
 }
