3ec90559025bbf6fd3a914384fad935f74757cd5
Alfred King <AK246749@falmouth.ac.uk>
2022-03-13 16:20:12 +0000

    changed lighting in scene
    made a postbox cloning machine

--- Assets/Scripts/Enviroment/GrabbedEnviromentReplacer.cs
+++ Assets/Scripts/Enviroment/GrabbedEnviromentReplacer.cs
@@ -6,7 +6,7 @@ public class GrabbedEnviromentReplacer : MonoBehaviour
 {
     public GameObject newReplacingGameObject;
     public Rigidbody Replace() {
-        
+
         GameObject newGameObject = Instantiate(newReplacingGameObject, transform.position, transform.rotation);
         Rigidbody newGameObjectRB = newGameObject.GetComponent<GrabbingEnviroment>().GrabObject();
         Destroy(newGameObject.GetComponent<GrabbingEnviroment>());
