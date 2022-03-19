d84f13a2cad383f00c69778c18b4be3c0781b5e8
Rhys Lampard <rhyslampard@gmail.com>
2022-03-19 18:41:39 +0000

    Random Menu Fun
    -Modified menu script to select a random song and to swap the 3D background depending on the song theme
    -Added height mapper thing to allow me to add a height map to one of the menu terrain concepts
    Obviously the backgrounds don't look amazing, so if someone wants to spruce them up, feel free, no offense will be taken

--- Assets/Scripts/Audio Scripts/Menu/AudioDontDestroyOnLoadWithFadeOutScript.cs
+++ Assets/Scripts/Audio Scripts/Menu/AudioDontDestroyOnLoadWithFadeOutScript.cs
@@ -11,15 +11,67 @@ public class AudioDontDestroyOnLoadWithFadeOutScript : MonoBehaviour
 
     public GameObject FadeInAudio;
 
+
+    public AudioClip Valiente;
+
+    public AudioClip DistantValleys;
+
+    public AudioClip TheLiberator;
+
+    public AudioSource MenuMusicSource;
+
+    public int MenuSelector;
+
+    public GameObject ValienteScene;
+
+    public GameObject DistantValleysScene;
+
+    public GameObject TheLiberatorScene;
+
+
     public static AudioDontDestroyOnLoadWithFadeOutScript instance;
+
+    void start()
+    {
+        MenuMusicSource = GetComponent<AudioSource>();
+    }
 
     void Awake()
     {
-        if (instance != null) {
+        MenuSelector = Random.Range(0, 3);
+
+        if (MenuSelector == 0)
+        {
+            MenuMusicSource.PlayOneShot(Valiente);
+            ValienteScene.SetActive(true);
+            DistantValleysScene.SetActive(false);
+            TheLiberatorScene.SetActive(false);
+
+        }
+
+        if (MenuSelector == 1)
+        {
+            MenuMusicSource.PlayOneShot(DistantValleys);
+            ValienteScene.SetActive(false);
+            DistantValleysScene.SetActive(true);
+            TheLiberatorScene.SetActive(false);
+        }
+
+        if (MenuSelector == 2)
+        {
+            MenuMusicSource.PlayOneShot(TheLiberator);
+            ValienteScene.SetActive(false);
+            DistantValleysScene.SetActive(false);
+            TheLiberatorScene.SetActive(true);
+        }
+
+
+
+        if (instance != null) 
+        {
             Destroy(instance.gameObject);
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
-
 }
