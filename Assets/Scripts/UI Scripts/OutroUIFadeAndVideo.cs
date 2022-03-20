d6c68ca039295474037bcc2a1b010f521f09b870
Rhys Lampard <rhyslampard@gmail.com>
2022-03-20 17:48:40 +0000

    Asynchronous scene loading
    -Modified 'SceneAfterVideoFinish' & 'OutroUIFadeAndVideo' scripts to allow for background scene loading to reduce / remove scene load times during credits & intro splash
    
    (Will look to add this to the cutscene section so that main NewLevel scene will transition faster)

--- Assets/Scripts/UI Scripts/OutroUIFadeAndVideo.cs
+++ Assets/Scripts/UI Scripts/OutroUIFadeAndVideo.cs
@@ -19,6 +19,12 @@ public class OutroUIFadeAndVideo : MonoBehaviour
     public GameObject HUD;
 
 
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+
     void Update()
     {
         FadeTimer += Time.deltaTime;
@@ -59,7 +65,43 @@ public class OutroUIFadeAndVideo : MonoBehaviour
         if (FadeTimer > 52)
         {
             //HUD.SetActive(true);
-            SceneManager.LoadScene(1);
+            //SceneManager.LoadScene(1);
         }
     }
+
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        //Begin to load the Scene you specify
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Menu");
+        //Don't let the Scene activate until you allow it to
+        asyncOperation.allowSceneActivation = false;
+        //Debug.Log("Pro :" + asyncOperation.progress);
+        //When the load is still in progress, output the Text and progress bar
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            //m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                //Change the Text to show the Scene is ready
+                //m_Text.text = "100%";
+                //Wait for intro to finish before activating the Scene
+                if (FadeTimer > 52)
+                {
+                    //Activate the Scene
+                    asyncOperation.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+
+
 }
