34b0df80cf0b007eebd19b5f46e9805cbacc6ddd
GA246525 <ga246525@falmouth.ac.uk>
2022-04-04 22:01:56 +0100

    Hid the ragdollColliders and ragdollRigidbodies arrays

--- Assets/Scripts/Enemy/RagdollController.cs
+++ Assets/Scripts/Enemy/RagdollController.cs
@@ -12,9 +12,9 @@ public class RagdollController : MonoBehaviour
 
     private Transform rigPositionOffset; 
 
-    //[HideInInspector]
+    [HideInInspector]
     public Collider[] ragdollColliders; // all colliders used for the ragdoll in the joints
-    //[HideInInspector]
+    [HideInInspector]
     public Rigidbody[] ragdollRigidbodies; // all rigidbodies used for the ragdoll in the joints
 
     public GameObject rig; // rig of the character
