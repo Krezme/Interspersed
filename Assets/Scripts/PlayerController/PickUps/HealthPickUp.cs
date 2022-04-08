c2bceb9ee1bb2d3aa0fa59bdb9ede4da93071543
Rhys Lampard <rhyslampard@gmail.com>
2022-04-08 02:14:08 +0100

    Health Crystal
    -Added health crystal area sound
    -Added health crystal pickup sound via 2D audiosource in Audio parent onbject
    -Changed light above health crystal to green

--- Assets/Scripts/PlayerController/PickUps/HealthPickUp.cs
+++ Assets/Scripts/PlayerController/PickUps/HealthPickUp.cs
@@ -6,12 +6,15 @@ public class HealthPickUp : MonoBehaviour
 {
     public float heal;
 
+    public AudioSource SFXHealthPickup;
+
     public void OnTriggerEnter (Collider other) {
         if (other.gameObject.tag == "Player") {
             Debug.Log(other);
             if (other.gameObject.TryGetComponent<PlayerStatisticsManager>(out PlayerStatisticsManager playerStatisticsManager)) { 
                 if (playerStatisticsManager.maxStatistics.health > playerStatisticsManager.currentStatistics.health) {
                     playerStatisticsManager.HealthRestore(heal);
+                    SFXHealthPickup.Play(); 
                     Destroy(this.gameObject);
                 }
             }
