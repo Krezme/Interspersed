3bbb78f075d4d935266f1a3434b6728c27c8a3e5
GA246525 <ga246525@falmouth.ac.uk>
2022-04-20 23:08:15 +0100

    Enemy item drop functionality finished

--- Assets/Scripts/Enemy/Fiew of View/EnemyDrops.cs
+++ Assets/Scripts/Enemy/Fiew of View/EnemyDrops.cs
@@ -13,26 +13,31 @@ public class EnemyDrops : MonoBehaviour
     public ItemPool itemPool;
     public NumberOfDropsChance[] numberOfDrops;
 
-    private List<GameObject> itemsWithEntries;
+    private List<GameObject> itemsWithEntries = new List<GameObject>();
 
-    private List<int> numberOfDropsChance;
+    private List<int> numberOfDropsChance = new List<int>();
 
     void Start () {
         foreach (GameObjectPool gOPool in itemPool.pool) {
-            for (int i = 0; i < gOPool.numberOfEntries; ++i) {
+            for (int i = 0; i < gOPool.numberOfEntries; i++) {
                 itemsWithEntries.Add(gOPool.item);
             }
         }
         foreach (NumberOfDropsChance nODC in numberOfDrops) {
-            for (int i = 0; i < nODC.numberOfEntries; ++i) {
+            for (int i = 0; i < nODC.numberOfEntries; i++) {
                 numberOfDropsChance.Add(nODC.numberOfDrops);
             }
         }
     }
 
-    
+    public void RandomiseDrops() {
+        int rndNumberOfDrops = numberOfDropsChance[Random.Range(0, numberOfDropsChance.Count)];
+        for (int i = 0; i < rndNumberOfDrops; i++) {
+            Instantiate(RandomItem(), this.gameObject.transform.position, Quaternion.identity);
+        }
+    }
 
-    public GameObject RandomItemDrops() {
+    public GameObject RandomItem() {
         return itemsWithEntries[Random.Range(0, itemsWithEntries.Count)];
     }
 }
