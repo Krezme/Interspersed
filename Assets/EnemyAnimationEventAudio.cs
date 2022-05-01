7176ea998a78975e00f2a513feaf59713e8332fb
Rhys Lampard <rhyslampard@gmail.com>
2022-05-01 03:03:30 +0100

    Enemy sounds
    -Added 35 new Warpling death sounds
    -Added Carrier Spit sounds
    -Added Carrier Death sounds
    -Added Carrier Ragdoll sounds
    -Added Carrier Damaged sounds
    -Edited Enemy Animation Event Script to add in try & catch, along with new references to the RandomAudioPlayerV2

--- Assets/EnemyAnimationEventAudio.cs
+++ Assets/EnemyAnimationEventAudio.cs
@@ -8,15 +8,51 @@ public class EnemyAnimationEventAudio : MonoBehaviour
 
    public RandomAudioPlayer reload;
 
+   public RandomAudioPlayerV2 soundBank;
+
 
    void ShootSound()
    {
+       try
+       {
        shoot.PlayRandomClip();
+       }
+       catch{}
    }
    
-
     void ReloadSound()
     {
+        try
+        {
         reload.PlayRandomClip();
+        }
+        catch{}
+    }
+
+    void SpitSound()
+    {
+        try
+        {
+        soundBank.PlayRandomClip(defaultBankIndex: 0);
+        }
+        catch{}
+    }
+
+    void DamageSound()
+    {
+        try
+        {
+        soundBank.PlayRandomClip(defaultBankIndex: 1);
+        }
+        catch{}
+    }
+
+    void DeathSound()
+    {
+        try
+        {
+        soundBank.PlayRandomClip(defaultBankIndex: 2); 
+        }
+        catch{}
     }
 }
