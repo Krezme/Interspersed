4483dad802c1f8cc2436aec2ab3f00658ce5f34a
GA246525 <ga246525@falmouth.ac.uk>
2022-05-01 02:14:35 +0100

    Very simple Quest Marker

---
+++ Assets/Scripts/UI Scripts/QuestMarkerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestMarkerController : MonoBehaviour
+{
+    #region Singleton
+
+    public static QuestMarkerController instance;
+
+    void Awake () {
+        if (instance != null) {
+            Debug.LogError("THERE ARE TWO OR MORE QuestMarkerController INSTANCES! PLEASE KEEP ONLY ONE instance OF THIS SCRIPT");
+        }else {
+            instance = this;
+        }
+    }
+
+    #endregion
+
+    public Image image;
+
+    public Transform targetTransform;
+
+    public Vector2 clampPositionRatio = new Vector2(3, 27);
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newImagePosition = Camera.main.WorldToScreenPoint(targetTransform.position);
+        if (Mathf.Abs(newImagePosition.z) / newImagePosition.z > 0) {
+            image.rectTransform.position = new Vector3(Mathf.Clamp(newImagePosition.x, Camera.main.pixelWidth / clampPositionRatio.x, Camera.main.pixelWidth - (Camera.main.pixelWidth / clampPositionRatio.x)), Camera.main.pixelHeight - (Camera.main.pixelHeight / clampPositionRatio.y), 0);
+        }
+        
+        
+    }
+}
